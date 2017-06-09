8ca222c65b0c68d1e5bd51f24e1d64e9171ba5ee
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-06-09 07:06:43 -0400

    Travis build script

--- build.sh
+++ build.sh
@@ -1,9 +1,37 @@
 #!/usr/bin/env bash
-
 set -e
 
-# Collect bibtex entries
-bundle exec ruby ./_scripts/collect-bibtex.rb
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
+    echo "Skipping deploy; just doing a build."
+    # Collect bibtex entries
+    bundle exec ruby ./_scripts/collect-bibtex.rb
+    bundle exec jekyll build
+    # bundle exec htmlproofer ./_site
+    exit 0
+fi
 
-bundle exec jekyll build
-bundle exec htmlproofer ./_site
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Run deployment steps
+git checkout gh-pages || git checkout --orphan gh-pages
+
+git config user.name "CI auto deploy"
+git config user.email $COMMIT_AUHOR_EMAIL
+
+git add .
+git commit -m "Auto deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Push to gh-pages
+git push $SSH_REPO gh-pages --force
