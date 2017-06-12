b9e5547cb32fb0b737d5317cf1b21331d96a3696
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-06-12 01:10:33 -0400

    Keyword generator

--- _plugins/publications.rb
+++ _plugins/publications.rb
@@ -10,8 +10,20 @@ module Publications
         bib_entries = BibTeX.open bib_file
 
         publications = site.pages.detect { |page| page.name == 'publications.html' }
+        publications.data['bib_tags'] = get_tags(bib_entries)
         publications.data['bib_entries'] = bib_entries.map { |e| parse_entry e }
       end
+    end
+
+    def get_tags(bib_entries)
+      tags = Set.new
+      bib_entries.each do |entry|
+        begin
+          tags.merge entry['keywords'].split(',').map { |kw| kw.downcase.strip }
+        rescue
+        end
+      end
+      Array(tags)
     end
 
     def parse_entry(entry)
