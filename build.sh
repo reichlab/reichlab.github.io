b9ab3cc1c2e46f066638f3567844f981fde6974f
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-07-08 00:42:30 -0400

    logging for debuggin

--- build.sh
+++ build.sh
@@ -24,6 +24,9 @@ git commit -m "Auto deploy to GitHub Pages: ${SHA}"
 git checkout -b gh-pages
 git remote add ghp $REPO
 
+git status
+git remote show ghp
+
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
