0eb131c9458c242f6bc59d8a1913d9783b67b2fd
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2017-07-09 00:00:27 -0400

    Support 'type' in people

--- _plugins/people.rb
+++ _plugins/people.rb
@@ -10,6 +10,10 @@ module People
 
       people = site.pages.detect { |page| page.name == 'people.html' }
       people.data['members'] = team_data.map { |tm| parse_member_entry(tm, site.config['baseurl']) }
+      uniq_types = people.data['members'].map { |tm| tm['type'] }.uniq.sort
+      # Keep 'default' at top
+      uniq_types.insert(0, uniq_types.delete_at(uniq_types.index('default')))
+      people.data['types'] = uniq_types
     end
 
     def get_full_url(url, base)
@@ -22,6 +26,9 @@ module People
       entry['links'] = entry['links'].map do |link|
         link['url'] = get_full_url(link['url'], base)
         link
+      end
+      unless entry.key?('type')
+        entry['type'] = 'default'
       end
       entry
     end
